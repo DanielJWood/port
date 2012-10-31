5fcd716a202846bef7f7b9a071ac94f02c8dbe3b
Daniel Wood <dan@developmentseed.org>
2012-10-30 23:11:56 -0400

    changed zoom start

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
   var map = mapbox.map('map');
-    map.centerzoom({lat:30.022, lon: -89.45},8); 
+    map.centerzoom({lat:37.389, lon: -57.949},5); 
      map.addLayer(mapbox.layer().id('dw2.map-gozz62j7'));
               map.ui.hash.add();
 
