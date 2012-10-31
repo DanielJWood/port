126d850ed764bc83acc6e2405a26a2950a3293fc
Daniel Wood <dan@developmentseed.org>
2012-10-30 23:29:00 -0400

    adjust hash in js. Change copy

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
   var map = mapbox.map('map');
     map.centerzoom({lat:37.389, lon: -57.949},5); 
      map.addLayer(mapbox.layer().id('dw2.map-gozz62j7'));
-              map.ui.hash.add();
+            //  map.ui.hash.add();
 
