8dea3735c6a038363c57fda088f7f19e3a403e40
Daniel Wood <daniel.j.wood11@gmail.com>
2015-03-31 10:52:28 -0400

    comment code

--- site.js
+++ site.js
@@ -1,8 +1,10 @@
+// initiate map layers
   var map = mapbox.map('map');
     map.centerzoom({lat:37.389, lon: -57.949},5); 
     map.addLayer(mapbox.layer().id('dw2.map-gozz62j7'));
             //  map.ui.hash.add();
 
+// Set locations of zoom carosel
 var data = [
   {
     "lat":-2.943,
